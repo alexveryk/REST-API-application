69264b8d1dd8ee27fff95457e9300c67cb1e18f4
alexveryk <veryk.av@gmail.com>
2023-05-30 23:58:06 +0300

    Contacts have been filtered by the favorite field

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -17,9 +17,11 @@ const favoriteSchema = Joi.object({
 const getAll = async (req, res, next) => {
   try {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+
+    const { page = 1, limit = 10, favorite = [true, false] } = req.query;
+
     const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner }, "", { skip, limit });
+    const result = await Contact.find({ owner, favorite }, "", { skip, limit });
     console.log("ALL");
     res.json(result);
   } catch (error) {
